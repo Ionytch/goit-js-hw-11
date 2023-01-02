d8e1836e85f8610c20483e530960ca8b03dd5fbd
Aleks Ionin <starik.ionytch@gmail.com>
2023-01-02 23:54:45 +0200

    files index.js, index.scss updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // const { Notify } = require("notiflix");
 
-// import axios from "axios";
+import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import SimpleLightbox from "simplelightbox";
@@ -8,25 +8,22 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const API_KEY = '32152184-2ad461e647b19751df8bc3af5';
 
-let currentPage = 1;
+let currentPage;
 let totalPages = undefined;
-const perPage = 4;
+const perPage = 40;
 
 const searchField = document.getElementById('search-form'); 
 const baseURL = 'https://pixabay.com/api/?key=';
 const searchParameters = 'image_type=photo&orientation=horizontal&safesearch=true';
 const loadMoreBtn = document.querySelector('.load-more');
 
-// const articlesContainer = document.getElementById('articles'); // контейнер для статей
-// const paginationContainer = document.getElementById('pagination'); // контейнер для пагінації
-// const loadMoreBtnRef = document.getElementById('loadMore'); // кнопка підгрузки іще
 let keyWord = '';
-let currentHits = 0;
 
 const imagesContainer = document.querySelector('.gallery');
 
 searchField.addEventListener("submit", e => {
   e.preventDefault();
+  currentPage = 1;
    keyWord = e.currentTarget.searchQuery.value;
   console.log(keyWord);
   if (keyWord === '') {
@@ -34,108 +31,80 @@ searchField.addEventListener("submit", e => {
     return;
   }
   getImages({keyWord})
-    // .then((hits) => render(hits))
-    // .catch((error) => console.log(error));
   })
 
 
-function getImages({keyWord, currentPage}) {
-  fetch(`${baseURL}${API_KEY}&q=${keyWord}&${searchParameters}&per_page=${perPage}&page=${currentPage}`)
-    .then(res => {
-    if (res.status !== 200){
-    throw new Error(res.message)
-    }
-    return res.json(); 
-    })   
-    .then(({ hits, totalHits }) => {
-      if (totalHits === 0) {
+
+
+
+async function getImages() {
+  try {
+    const response = await axios.get(`${baseURL}${API_KEY}&q=${keyWord}&${searchParameters}&per_page=${perPage}&page=${currentPage}`);
+    console.log(response.data);
+     console.log(response.data.hits);
+    console.log(response.data.totalHits);
+        console.log(response.data.hits.length);
+          if (response.data.totalHits === 0) {
         Notify.warning("Sorry, there are no images matching your search query. Please try again.");
-      }
-      currentHits += hits.length;
-      console.log(hits);
-      console.log(totalHits);
-      render(hits);
-      calculatePagination(totalHits)
-    })
-        
+    }
+    render(response.data.hits);
+    calculatePagination(response.data.totalHits);
+    onSimpleLightBox();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function calculatePagination(totalHits) { 
   totalPages = Math.ceil(totalHits / perPage);
   console.log(totalPages);
   if (totalHits > perPage) {
-    loadMoreBtn.classList.remove('is-hidden');
-    // loadMoreBtn.classList.add('is-hidden');
+    loadMoreBtn.classList.remove('is-hidden');    
   }
-  
-  
-  
-    
+      
 }
-
-
 
 loadMoreBtn.addEventListener("click", e => {
   currentPage += 1;
-  const responce=getImages({ keyWord, currentPage });
-  
-
-  if (currentHits === responce.totalHits) {
-    loadMoreBtn.classList.add('is-hidden');
     console.log(currentPage);
     console.log(totalPages);
+  if (currentPage >= totalPages) {
+    loadMoreBtn.classList.add('is-hidden');
     Notify.info("We're sorry, but you've reached the end of search results.");
-    
+    return
   }
-  
+  getImages({ currentPage });
 })
 
 function render(hits) {
   imagesContainer.innerHTML = '';
   hits.forEach(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-    const imagesEl = `<div class="photo-card">
-  <a class="gallery__link" href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  </a> <div class="info">
+    const imagesEl = `<div class="photo-card"><a class="gallery__link" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
+  <div class="info">
     <p class="info-item">
-      <b>${likes}</b>
+      Likes:<b>${likes}</b>
     </p>
     <p class="info-item">
-      <b>${views}</b>
+      Views:<b>${views}</b>
     </p>
     <p class="info-item">
-      <b>${comments}</b>
+      Comments:<b>${comments}</b>
     </p>
     <p class="info-item">
-      <b>${downloads}</b>
+      Downloads:<b>${downloads}</b>
     </p>
   </div>
-</div>`;
+  </div>
+  `;
     imagesContainer.insertAdjacentHTML('beforeend', imagesEl)
   });
 }
   
-
-
-// const galleryContainer = document.querySelector('.gallery');
-
-// const markup=galleryItems.map((item)=>` <div class="gallery__item">
-//   <a class="gallery__link" href="${item.original}">
-//     <img
-//       class="gallery__image"
-//       src="${item.preview}"
-//       data-source="${item.original}"
-//       alt="${item.description}"
-//     />
-//   </a>
-// </div> `).join("");
-// console.log (markup);
-// galleryContainer.insertAdjacentHTML("beforeend", markup);
-
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function (e) {
-console.log(e);  });
-
-gallery.on('error.simplelightbox', function (e) {
-  console.log(e);
-});
-
+function onSimpleLightBox() {
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
